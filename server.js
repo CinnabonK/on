5825f8f7b737d2c55c52f59add595ffd774c37d5
kikunohana <138283495+CinnabonK@users.noreply.github.com>
2024-10-14 16:06:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 3000 });
+
 
 server.on('connection', ws => {
     ws.on('message', message => {
